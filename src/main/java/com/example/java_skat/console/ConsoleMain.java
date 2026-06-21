6148545ae868757abc5926ad0a9c625bea8bdb00
MarcinPietrzyk255675 <255675@edu.p.lodz.pl>
2026-06-21 19:19:25 +0200

    pozycje i liczba rozdań

--- src/main/java/com/example/java_skat/console/ConsoleMain.java
+++ src/main/java/com/example/java_skat/console/ConsoleMain.java
@@ -4,16 +4,22 @@ import com.example.java_skat.game.GameController;
 import com.example.java_skat.game.PlayerId;
 
 public class ConsoleMain {
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		GameController gameController = new GameController();
-		gameController.dealCards();
+		for (int i = 0; i < 3; i++) {
 
-		for (PlayerId player : PlayerId.values()){
-			System.out.println(player.getDisplayName() + ": ");
-			gameController.getDealState().getHand(player).forEach(System.out::println);
+
+			gameController.dealCards();
+
+			System.out.println("Deal " + (i + 1) + "/" + GameController.getMaxDeals());
+			for (PlayerId player : PlayerId.values()) {
+				System.out.println(player.getDisplayName() + "-" +
+				                   gameController.getDealState().getPosition(player).displayName() + ": ");
+				gameController.getDealState().getHand(player).forEach(System.out::println);
+			}
+
+			System.out.println("Skat: ");
+			gameController.getDealState().getSkat().forEach(System.out::println);
 		}
-
-		System.out.println("Skat: ");
-		gameController.getDealState().getSkat().forEach(System.out::println);
 	}
 }
--- src/main/java/com/example/java_skat/game/GameController.java
+++ src/main/java/com/example/java_skat/game/GameController.java
@@ -8,12 +8,23 @@ public class GameController {
 
 	private static final int HAND_SIZE = 10;
 	private static final int SKAT_SIZE = 2;
+	private static final int MAX_DEALS = 12;
 
 	private DealState dealState;
 
+
+	private int dealNumber = 0;
+
 	public void dealCards() {
+		if (dealNumber >= MAX_DEALS) {
+			throw new IllegalStateException("Max deals reached");
+		}
+
+		dealNumber++;
 		List<Karta> deck = DeckFactory.createShuffledDeck();
 		dealState = new DealState();
+
+		assignPositions();
 
 		dealState.getHand(PlayerId.PLAYER_1).addAll(deck.subList(0, HAND_SIZE));
 		dealState.getHand(PlayerId.PLAYER_2).addAll(deck.subList(HAND_SIZE, HAND_SIZE * 2));
@@ -24,4 +35,21 @@ public class GameController {
 	public DealState getDealState() {
 		return dealState;
 	}
+
+	public int getDealNumber() {
+		return dealNumber;
+	}
+
+	public static int getMaxDeals() {
+		return MAX_DEALS;
+	}
+
+	private void assignPositions() {
+		PlayerId[] players = PlayerId.values();
+		int shift = (dealNumber - 1) % players.length;
+		dealState.setPosition(players[shift], PlayerPosition.FOREHAND);
+		dealState.setPosition(players[(shift + 1) % players.length], PlayerPosition.MIDDLEHAND);
+		dealState.setPosition(players[(shift + 2) % players.length], PlayerPosition.REARHAND);
+	}
+
 }
