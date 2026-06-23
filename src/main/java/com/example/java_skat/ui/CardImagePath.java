6a71a906f241bab1fd19d7fc31c513e4e5986b64
MarcinPietrzyk255675 <255675@edu.p.lodz.pl>
2026-06-23 06:43:26 +0200

    ścieżki obrazów kart

---
+++ src/main/java/com/example/java_skat/ui/CardImagePath.java
@@ -0,0 +1,40 @@
+package com.example.java_skat.ui;
+
+import pl.skat.core.Figura;
+import pl.skat.core.Karta;
+import pl.skat.core.Kolor;
+
+final class CardImagePath {
+    private CardImagePath() {
+    }
+
+    static String forCard(Karta card) {
+        return "/Cards/" + suitName(card.kolor()) + "_" + rankName(card.figura()) + ".png";
+    }
+
+    static String back() {
+        return "/Cards/card_back.png";
+    }
+
+    private static String suitName(Kolor kolor) {
+        return switch (kolor) {
+            case TREFL -> "card_clubs";
+            case PIK -> "card_spades";
+            case SERCE -> "card_hearts";
+            case DZWONEK -> "card_diamonds";
+        };
+    }
+
+    private static String rankName(Figura figura) {
+        return switch (figura) {
+            case AS -> "A";
+            case KROL -> "K";
+            case KROLOWA -> "Q";
+            case JOPEK -> "J";
+            case DZIESIATKA -> "10";
+            case DZIEWIATKA -> "09";
+            case OSEMKA -> "08";
+            case SIODEMKA -> "07";
+        };
+    }
+}
